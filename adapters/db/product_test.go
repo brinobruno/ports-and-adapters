a2ac84b92684a01ac396fc2195122b8de6e0da0b
Bruno Correa <brunosantos6ft@gmail.com>
2025-08-27 20:26:45 -0300

    feat: implement db and db test

---
+++ adapters/db/product_test.go
@@ -0,0 +1,54 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/codeedu/go-hexagonal/adapters/db"
+	"github.com/stretchr/testify/require"
+)
+
+var Db *sql.DB
+
+func setup() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+		"id" string,
+		"name" string,
+		"price" float,
+		"status" string
+	);`
+
+	statement, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `insert into products values("abc", "Product test", 0, "disabled")`
+	statement, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec()
+}
+
+func TestProductDbGet(t *testing.T) {
+	setup()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+}
