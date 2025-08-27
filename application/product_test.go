d2bf89c322fb3f76dbeb9e352e087acc5f91cf1c
Bruno Correa <brunosantos6ft@gmail.com>
2025-08-27 15:39:15 -0300

    feat: implement enable/disable and validation with unit tests

--- application/product_test.go
+++ application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/codeedu/go-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,6 +22,55 @@ func TestProductEnable(t *testing.T) {
 	require.Equal(
 		t,
 		"price must be greater than 0 to enable the product",
+		err.Error(),
+	)
+}
+
+func TestProductDisable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(
+		t,
+		"price must be 0 to disable the product",
+		err.Error(),
+	)
+}
+
+func TestProductIsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "Invalid"
+	_, err = product.IsValid()
+	require.Equal(
+		t,
+		"status must be enabled or disabled",
+		err.Error(),
+	)
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(
+		t,
+		"price must be greater than 0",
 		err.Error(),
 	)
 }
